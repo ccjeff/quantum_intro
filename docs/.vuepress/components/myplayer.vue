f47de9949c248f93e2e5cd1cd309a46d1a801122
Endlessio <117010423@link.cuhk.edu.cn>
2020-12-11 02:44:01 +0800

    vue update

---
+++ docs/.vuepress/components/myplayer.vue
@@ -0,0 +1,23 @@
+<script>
+  import AudioPlayer from './audio-player.vue'
+
+  export default {
+    components: {
+      AudioPlayer
+    },
+
+    data () {
+      return {
+        audioSources: [
+          "docs/.vuepress/public/wonderwall.mp3"
+        ]
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <audio-player :sources="audioSources" :loop="true"></audio-player>
+  </div>
+</template>
